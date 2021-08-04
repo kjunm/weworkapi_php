0654967d0c3bb120e8a124fe1db72a09a5c4a433
wangjm1218 <wangjm1404397383@gmail.com>
2021-08-05 00:51:04 +0800

    fix

--- utils/ResponseUtils.php
+++ utils/ResponseUtils.php
@@ -2,7 +2,7 @@
 
 class ResponseUtils
 {
-    public static function result($success = true, $error_code = 200, $message = "", $data = [])
+    public static function success($data = [], $message = "")
     {
         return json_encode(
             [
@@ -18,7 +18,32 @@ class ResponseUtils
                     "Access-Control-Max-Age" => 600
                 ],
                 "body" => [
-                    "success" => $success,
+                    "success" => true,
+                    "error_code" => 200,
+                    "message" => $message,
+                    "data" => $data
+                ]
+            ]
+        );
+    }
+
+    public static function fail($message = "", $error_code = 1, $data = [])
+    {
+        return json_encode(
+            [
+                "isBase64Encoded" => "false",
+                "statusCode" => "200",
+                "headers" => [
+                    "Access-Control-Allow-Origin" => "*",
+                    "Access-Control-Allow-Credentials" => true,
+                    "Access-Control-Allow-Headers" =>
+                    "X-Requested-With,content-type,auth-token,X-Log-Id",
+                    "Access-Control-Allow-Methods" =>
+                    "GET,POST,PUT,DELETE,HEAD,OPTIONS,PATCH",
+                    "Access-Control-Max-Age" => 600
+                ],
+                "body" => [
+                    "success" => false,
                     "error_code" => $error_code,
                     "message" => $message,
                     "data" => $data
